75fcee00547a3f70f46058f350e9670acea65171
Davit Andzavelidze <davit.andzavelidze@gmail.com>
2022-05-17 16:12:41 +0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,72 @@
+let nameError = document.querySelector(".name-error");
+let phoneError = document.querySelector(".phone-error");
+let emailError = document.querySelector(".email-error");
+let subjectError = document.querySelector(".subject-error");
+let submitError = document.querySelector(".submit-error");
+let validateCheck = document.querySelector("#validateCheck");
+let inputSuccess = document.querySelector(".input");
+
+// -----------------Name Validation-----------------
+const nameValidation = function validateName() {
+  let name = document.getElementById("contact-name").value;
+
+  if (name.length == 0) {
+    nameError.innerHTML = "სახელის ჩაწერა აუცილებელია";
+    return false;
+  }
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerHTML = "ჩაწერეთ სრულად";
+    return false;
+  }
+  nameError.innerHTML = '<i class="fas fa-check checkmark"></i>';
+  return true;
+};
+
+// -----------------Phone Validation-----------------
+const phoneValidation = function validatePhone() {
+  let phone = document.getElementById("contact-phone").value;
+
+  if (phone.length == 0) {
+    phoneError.innerHTML = "მობილურის ნომერი აუცილებელია";
+    return false;
+  }
+  if (phone.length !== 9) {
+    phoneError.innerHTML = "მობილურის ნომერი უნდა იყოს 9 ციფრი";
+    return false;
+  }
+  if (!phone.match(/^[0-8]{9}$/)) {
+    phoneError.innerHTML = "მხოლოდ რიცხვები";
+    return false;
+  }
+  phoneError.innerHTML = '<i class="fas fa-check checkmark"></i>';
+  return true;
+};
+
+// -----------------Email Validation-----------------
+const emailValidation = function validateEmail() {
+  let email = document.getElementById("contact-email").value;
+
+  if (email.length == 0) {
+    emailError.innerHTML = "მეილი აუცილებელია";
+    return false;
+  }
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerHTML = "მეილი არასწორია";
+    return false;
+  }
+  emailError.innerHTML = '<i class="fas fa-check checkmark"></i>';
+  return true;
+};
+
+// Check input if there are unfill fields appear message to fix
+
+const checkValidate = validateCheck.addEventListener("click", function () {
+  if (!nameValidation() || !phoneValidation() || !emailValidation()) {
+    submitError.style.display = "block";
+    submitError.innerHTML = "გთხოვთ გაასწორეთ შეცდომები";
+    setTimeout(function () {
+      submitError.style.display = "none";
+    }, 3000);
+    return false;
+  }
+});
